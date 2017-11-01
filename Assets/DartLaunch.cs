ce02c6562aa4b98988a9e579094aa22034885328
Lakshya Goel <goel0072@umn.edu>
2017-11-01 14:49:47 -0500

    Added Script to Dart

---
+++ Assets/DartLaunch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartLaunch : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public float strength = 1;
+	public Transform direction = null;
+
+	// Use this for initialization
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		if (this.direction != null) {
+			rb.AddForce (direction.rotation * strength, ForceMode.Impulse);
+		}
+	}
+}
