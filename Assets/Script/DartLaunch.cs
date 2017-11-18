a19b9060acd829f15f8696c752f6e6f5b8a89aba
Suppressed entry <goel0072@ad.umn.edu>
2017-11-18 12:43:49 -0600

    Integrated with SteamVR

--- Assets/Script/DartLaunch.cs
+++ Assets/Script/DartLaunch.cs
@@ -5,27 +5,40 @@ using UnityEngine;
 public class DartLaunch : MonoBehaviour {
 
 	private Rigidbody rb;
-	//public float strength = 1000000000000000;
+	public GameObject controllerRight;
+
+	private SteamVR_Controller.Device device;
+	private SteamVR_TrackedController controller;
+	private SteamVR_TrackedObject trackedObj;
+
 
 	// Use this for initialization
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
+		controller = controller.GetComponent<SteamVR_TrackedController> ();
+		controller.TriggerClicked += TriggerPressed;
+		trackedObj = controllerRight.GetComponent<SteamVR_TrackedObject> ();
 	}
-	
+
+	private void TriggerPressed(object sender, ClickedEventArgs e){
+		ThrowDart ();
+	}
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-	void FixedUpdate(){
-		Vector3 movement = new Vector3 (-1.0f, 0.0f, 0.0f);
+	void ThrowDart(){
+		//Vector3 movement = new Vector3 (-1.0f, 0.0f, 0.0f);
 
-		if(Input.GetKeyDown(KeyCode.Space)){
-			
-			//rb.AddForce(movement * strength, ForceMode.Impulse);
+		//if(Input.GetKeyDown(KeyCode.Space)){
 			rb.velocity = new Vector3(-20, 0, 0);
 			rb.useGravity = true;
-		}
+		//}
+	}
+
+	void FixedUpdate(){
+		
 	}
 
 	void OnCollisionEnter(Collision collision) {
